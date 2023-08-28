391d8453d0890ac3fcd7e9617848233b5adde9e1
Giles hinchcliff <giles@rebelastronaut.co.uk>
2023-08-28 11:56:11 +0100

    add schedule and fix pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ const Home = () => {
   function importAll(r) {
       r.keys().forEach((key) => (cache[key] = r(key)));
   }
-  importAll(require.context('../../public/astronaut-of-the-day/images', false, /\.(png|jpe?g|svg|WEBP)$/));
+  importAll(require.context('../../public/astronaut-of-the-day/images', false, /\.(png|jpe?g|svg|webp)$/));
   // @ts-ignore: Unreachable code error
   const images = Object.entries(cache).map(cache => cache[1].default);
   
